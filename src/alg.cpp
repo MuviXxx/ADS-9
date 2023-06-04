116364872458de98eb377dffec9b9c19262b42ae
MuviXxx <118690549+MuviXxx@users.noreply.github.com>
2023-06-04 22:27:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,23 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw std::string("Unable to open file!");
+  }
+  std::string word = "";
+  BST<std::string> bst;
+  while (!file.eof()) {
+    word = "";
+    while (1) {
+      char symbol = file.get();
+      if ((symbol >= 65 && symbol <= 90) || (symbol >= 97 && symbol <= 122)) {
+        word += tolower(symbol);
+      } else {
+        break;
+      }
+    }
+    bst.AddValue(word);
+  }
+  return bst;
 }
